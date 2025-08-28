973d6567b4c54641371e35dc3338daf9810b78da
Deepak Singh <deepakksingh1202@gmail.com>
2025-08-28 23:57:02 +0530

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div>
+      <h1 className='text-3xl text-blue-600'>note-taking application</h1>
+    </div>
+  )
+}
+
+export default App
+
